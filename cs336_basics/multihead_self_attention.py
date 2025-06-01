f3799dc22b2e0423fda465b7312d9f91d7880925
yusheng-ma <koala34025@gmail.com>
2025-06-01 16:27:26 +0800

    fix to proper einsum in multihead_self_attention

--- cs336_basics/multihead_self_attention.py
+++ cs336_basics/multihead_self_attention.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 from typing import Optional
-from jaxtyping import Float
+from jaxtyping import Float, Bool
 from einops import einsum, rearrange
 from cs336_basics.scaled_dot_product_attention import scaled_dot_product_attention
 
@@ -22,10 +22,11 @@ class CausalMultiHeadSelfAttention(nn.Module):
         d_k = d_model // num_heads
         d_v = d_model // num_heads
 
-        self.q_proj_weight: Float[Tensor, "d_k d_in"] = nn.Parameter(torch.empty(d_k * num_heads, d_model, device=device, dtype=dtype))
-        self.k_proj_weight: Float[Tensor, "d_k d_in"] = nn.Parameter(torch.empty(d_k * num_heads, d_model, device=device, dtype=dtype))
-        self.v_proj_weight: Float[Tensor, "d_v d_in"] = nn.Parameter(torch.empty(d_v * num_heads, d_model, device=device, dtype=dtype))
-        self.o_proj_weight: Float[Tensor, "d_model d_v"] = nn.Parameter(torch.empty(d_model, d_v * num_heads, device=device, dtype=dtype))
+        # d_in = d_model
+        self.q_proj_weight: Float[Tensor, "num_heads*d_k d_in"] = nn.Parameter(torch.empty(d_k * num_heads, d_model, device=device, dtype=dtype))
+        self.k_proj_weight: Float[Tensor, "num_heads*d_k d_in"] = nn.Parameter(torch.empty(d_k * num_heads, d_model, device=device, dtype=dtype))
+        self.v_proj_weight: Float[Tensor, "num_heads*d_v d_in"] = nn.Parameter(torch.empty(d_v * num_heads, d_model, device=device, dtype=dtype))
+        self.o_proj_weight: Float[Tensor, "d_model num_heads*d_v"] = nn.Parameter(torch.empty(d_model, d_v * num_heads, device=device, dtype=dtype))
 
     def forward(
             self,
@@ -41,22 +42,18 @@ class CausalMultiHeadSelfAttention(nn.Module):
         q_x = einsum(q_rearranged, x, "num_head d_k d_in, ... sequence_length d_in -> ... num_head sequence_length d_k")
         k_x = einsum(k_rearranged, x, "num_head d_k d_in, ... sequence_length d_in -> ... num_head sequence_length d_k")
         v_x = einsum(v_rearranged, x, "num_head d_v d_in, ... sequence_length d_in -> ... num_head sequence_length d_v")
-        # print("HERE", q_x.shape, k_x.shape, v_x.shape)
-        # print("LMAO", q_x)
-        # print("LOLL", k_x)
-        # print("QWER", v_x) # here same
 
         # create mask
-        mask = ~torch.triu(torch.ones(sequence_length, sequence_length), diagonal=1).bool()
-        # print("HERE", mask.shape)
-        # print("HERE", mask) # here same
-        out: Float[Tensor, "... num_head sequence_length d_in"] = scaled_dot_product_attention(q_x, k_x, v_x, mask)
-        # here same
-        # out2 = rearrange(out, "... num_head sequence_length d_in -> ... sequence_length (d_in num_head)", num_head=self.num_heads)
-        out2 = rearrange(out, "... num_head sequence_length d_in -> ... sequence_length (num_head d_in)", num_head=self.num_heads)
-        # print("HERE", out2.shape)
-        # print("HERE", out2) # same
+        mask: Bool[Tensor, "sequence_length sequence_length"] = ~torch.triu(torch.ones(sequence_length, sequence_length), diagonal=1).bool()
 
+        # d_v not d_in: d_v = d_in/num_head
+        out: Float[Tensor, "... num_head sequence_length d_v"] = scaled_dot_product_attention(q_x, k_x, v_x, mask)
+        
+        # notice the order
+        # out2 = rearrange(out, "... num_head sequence_length d_in -> ... sequence_length (d_in num_head)", num_head=self.num_heads)
+        out2 = rearrange(out, "... num_head sequence_length d_v -> ... sequence_length (num_head d_v)", num_head=self.num_heads)
+
+        # although o_proj is square, do not mess up d_model with num_heads*d_v
         # result = einsum(self.o_proj_weight, out2, "d_model d_v, ... sequence_length d_model -> ... sequence_length d_v")
-        result = einsum(self.o_proj_weight, out2, "d_model d_v, ... sequence_length d_v -> ... sequence_length d_model")
+        result = einsum(self.o_proj_weight, out2, "d_model num_heads_d_v, ... sequence_length num_heads_d_v -> ... sequence_length d_model")
         return result
