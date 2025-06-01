c7d91ad6304a9e0266517eca00d7f420376656f0
yusheng-ma <koala34025@gmail.com>
2025-06-01 16:08:34 +0800

    multihead_self_attention bad einsum, no linear, and not 1 matrix

---
+++ cs336_basics/multihead_self_attention.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+from typing import Optional
+from jaxtyping import Float
+from einops import einsum, rearrange
+from cs336_basics.scaled_dot_product_attention import scaled_dot_product_attention
+
+class CausalMultiHeadSelfAttention(nn.Module):
+    def __init__(
+            self,
+            d_model: int,
+            num_heads: int,
+            device: Optional[torch.device] = None,
+            dtype: Optional[torch.dtype] = None
+    ):
+        super().__init__()
+        assert d_model % num_heads == 0, "d_model must be multiple of num_heads"
+        
+        self.num_heads = num_heads
+
+        d_k = d_model // num_heads
+        d_v = d_model // num_heads
+
+        self.q_proj_weight: Float[Tensor, "d_k d_in"] = nn.Parameter(torch.empty(d_k * num_heads, d_model, device=device, dtype=dtype))
+        self.k_proj_weight: Float[Tensor, "d_k d_in"] = nn.Parameter(torch.empty(d_k * num_heads, d_model, device=device, dtype=dtype))
+        self.v_proj_weight: Float[Tensor, "d_v d_in"] = nn.Parameter(torch.empty(d_v * num_heads, d_model, device=device, dtype=dtype))
+        self.o_proj_weight: Float[Tensor, "d_model d_v"] = nn.Parameter(torch.empty(d_model, d_v * num_heads, device=device, dtype=dtype))
+
+    def forward(
+            self,
+            x: Float[Tensor, "... sequence_length d_in"]
+    ) -> Float[Tensor, "... sequence_length d_out"]:
+        
+        sequence_length = x.shape[-2]
+        # rearrange
+        q_rearranged = rearrange(self.q_proj_weight, "(num_head d_k) d_in -> num_head d_k d_in", num_head=self.num_heads)
+        k_rearranged = rearrange(self.k_proj_weight, "(num_head d_k) d_in -> num_head d_k d_in", num_head=self.num_heads)
+        v_rearranged = rearrange(self.v_proj_weight, "(num_head d_v) d_in -> num_head d_v d_in", num_head=self.num_heads)
+
+        q_x = einsum(q_rearranged, x, "num_head d_k d_in, ... sequence_length d_in -> ... num_head sequence_length d_k")
+        k_x = einsum(k_rearranged, x, "num_head d_k d_in, ... sequence_length d_in -> ... num_head sequence_length d_k")
+        v_x = einsum(v_rearranged, x, "num_head d_v d_in, ... sequence_length d_in -> ... num_head sequence_length d_v")
+        # print("HERE", q_x.shape, k_x.shape, v_x.shape)
+        # print("LMAO", q_x)
+        # print("LOLL", k_x)
+        # print("QWER", v_x) # here same
+
+        # create mask
+        mask = ~torch.triu(torch.ones(sequence_length, sequence_length), diagonal=1).bool()
+        # print("HERE", mask.shape)
+        # print("HERE", mask) # here same
+        out: Float[Tensor, "... num_head sequence_length d_in"] = scaled_dot_product_attention(q_x, k_x, v_x, mask)
+        # here same
+        # out2 = rearrange(out, "... num_head sequence_length d_in -> ... sequence_length (d_in num_head)", num_head=self.num_heads)
+        out2 = rearrange(out, "... num_head sequence_length d_in -> ... sequence_length (num_head d_in)", num_head=self.num_heads)
+        # print("HERE", out2.shape)
+        # print("HERE", out2) # same
+
+        # result = einsum(self.o_proj_weight, out2, "d_model d_v, ... sequence_length d_model -> ... sequence_length d_v")
+        result = einsum(self.o_proj_weight, out2, "d_model d_v, ... sequence_length d_v -> ... sequence_length d_model")
+        return result
--- tests/adapters.py
+++ tests/adapters.py
@@ -18,6 +18,7 @@ from cs336_basics.swiglu import SwiGLU
 from cs336_basics.rope import RotaryPositionalEmbedding
 from cs336_basics.softmax import softmax
 from cs336_basics.scaled_dot_product_attention import scaled_dot_product_attention
+from cs336_basics.multihead_self_attention import CausalMultiHeadSelfAttention
 
 def run_linear(
     d_in: int,
@@ -151,7 +152,15 @@ def run_multihead_self_attention(
         Float[Tensor, " ... sequence_length d_out"]: Tensor with the output of running your optimized, batched multi-headed attention
         implementation with the given QKV projection weights and input features.
     """
-    raise NotImplementedError
+    multihead_self_attention = CausalMultiHeadSelfAttention(d_model, num_heads)
+    state_dict = {
+        "q_proj_weight": q_proj_weight,
+        "k_proj_weight": k_proj_weight,
+        "v_proj_weight": v_proj_weight,
+        "o_proj_weight": o_proj_weight
+    }
+    multihead_self_attention.load_state_dict(state_dict)
+    return multihead_self_attention(in_features)
 
 
 def run_multihead_self_attention_with_rope(
